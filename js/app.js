c08483ada099ec3b98a1ef272d6eb6d5f539e3b5
Logan Rice <loganrice72@gmail.com>
2014-06-20 16:08:49 -0700

    quiz data to include list of objects

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 	
 	$('.btn-wrapper').click(function(){
-		fillInQuiz(quiz.one);
+		fillInQuiz(quiz[0].choices);
 		$(this).hide("slow");
 	});
 })
 
-var quiz = {
-	one: ["choice1", "choice2", "choice3", "choice4", "choice5"],
-	two: ["choice1", "choice2", "choice3", "choice4", "choice5"],
-	three: ["choice1", "choice2", "choice3", "choice4", "choice5"]
+var quiz = [
+	{ choices: ["hand stand", "top spin", "head spin", "pushup"],
+	correctAnswer: 3,
+	info: "Warning: Do not do this move on concrete… for obvious reasons. The headspin is probably the most iconic and most unnatural move in breakdancing. Although its origins are derived from capoeira, Kid freeze has claimed to have invented the ‘continuous headspin’. Today, the move has been mastered by bboys around the world. In fact Bboy Aichi, from Japan, holds the Guiness World record for 135 rotations in 1 minute."
 }
+]
 
 function fillInQuiz(choices) {
 	for(i=0; i<choices.length; i++){
