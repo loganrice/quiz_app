ce5002ebdcb5b04d9cd17408b97f9cc5d8f8e3d4
Logan Rice <loganrice72@gmail.com>
2014-06-20 14:31:10 -0700

    fill in quiz with choices

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 	
-
+	fillInQuiz(quiz.one);
 
 })
+
+var quiz = {
+	one: ["choice1", "choice2", "choice3", "choice4", "choice5"],
+	two: ["choice1", "choice2", "choice3", "choice4", "choice5"],
+	three: ["choice1", "choice2", "choice3", "choice4", "choice5"]
+}
+
+function fillInQuiz(choices) {
+	for(i=0; i<choices.length; i++){
+		$("#choiceList").append('<li class="choice">' + choices[i] +'</li>');
+	}
+}
+
