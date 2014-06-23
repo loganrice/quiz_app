0616c992c25c752156d5a797a59e17095258f125
loganrice <loganrice72@gmail.com>
2014-06-23 09:25:50 -0700

    images with questions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 var quiz = [
 	{ choices: ["hand stand", "top spin", "head spin", "pushup"],
-	correctAnswer: 3,
-	info: "Warning: Do not do this move on concrete… for obvious reasons. The headspin is probably the most iconic and most unnatural move in breakdancing. Although its origins are derived from capoeira, Kid freeze has claimed to have invented the ‘continuous headspin’. Today, the move has been mastered by bboys around the world. In fact Bboy Aichi, from Japan, holds the Guiness World record for 135 rotations in 1 minute."
+	correctAnswer: 2,
+	info: "Warning: Do not do this move on concrete… for obvious reasons. The headspin is probably the most iconic and most unnatural move in breakdancing. Although its origins are derived from capoeira, Kid freeze has claimed to have invented the ‘continuous headspin’. Today, the move has been mastered by bboys around the world. In fact Bboy Aichi, from Japan, holds the Guiness World record for 135 rotations in 1 minute.",
+	image: "headspin.gif"
 	},
 	{ choices: ["one", "two", "three", "four"],
 	correctAnswer: 3,
-	info: "Warning: Do not"
+	info: "Warning: Do not",
+	image: "jackhammers.gif"
 	}
 ]
 
@@ -39,15 +41,22 @@ function hasAnswer(choice) {
 	return quiz[CURRENTQUESTION - 1].correctAnswer == choice;
 }
 
-function fillInQuiz(choices) {
+function fillInQuiz(quizObject) {
+	var choices = quizObject[CURRENTQUESTION].choices;
+	var image = quizObject[CURRENTQUESTION].image;
+	var imagePath = "img/" + image
+
 	for(i=0; i<choices.length; i++){
 		var listItem = '<li class="choice" id=' + i + '>' + choices[i] + '</li>';
 		$("#choiceList").append(listItem);
 	}
+
+	$("#quizImage").attr("src", imagePath);
+
 }
 
 function nextQuestion() {
 	$('#choiceList > li').remove();
-	fillInQuiz(quiz[CURRENTQUESTION].choices);
+	fillInQuiz(quiz);
 	return CURRENTQUESTION++;
 }
